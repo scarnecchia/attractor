f2894f16b987659b8df0bc0ce1ad29f7ea6f239c
D. Scarnecchia <scarnecchia@gmail.com>
2026-02-14 23:18:15 -0500

    chore(agent): update tools barrel export
    - Exports all new tool executors: read-file, write-file, edit-file, shell, grep, glob
    - Makes tool factories and executors available from @attractor/agent/tools

--- packages/agent/src/tools/index.ts
+++ packages/agent/src/tools/index.ts
@@ -1,1 +1,7 @@
 export * from './dispatch.js';
+export * from './read-file.js';
+export * from './write-file.js';
+export * from './edit-file.js';
+export * from './shell.js';
+export * from './grep.js';
+export * from './glob.js';
